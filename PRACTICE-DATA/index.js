d3e1f1009e787c5b8af4895d1d8f6ce8b9752fef
StephenBrooks220413 <krammer439298@gmail.com>
2021-06-30 16:44:16 -0500

    practiced files

--- PRACTICE-DATA/index.js
+++ PRACTICE-DATA/index.js
@@ -1,7 +1,14 @@
-var x = "developer";
-var y = "john";
 
-function items(){
-    prompt(`Hi ${y}, are you the ${x}?`);
-    confirm("Your said yes?")
-}
+const txt = '{"name":"John", "age":30, "city":"New York"}'
+const obj = JSON.parse(txt);
+document.getElementById("demo").innerHTML = obj.name + ", " + obj.age;
+
+const text = '{"name":"John", "birth":"1986-12-14", "city":"New York"}';
+const obj1 = JSON.parse(text);
+obj.birth = new Date(obj1.birth);
+
+document.getElementById("info").innerHTML = obj1.name + ", " + obj1.birth;
+
+const info = {name: "john", age: 40, city: "Los Angelos"};
+const list = JSON.stringify(info);
+document.getElementById("list").innerHTML = info;
