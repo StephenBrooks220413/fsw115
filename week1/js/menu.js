1c436d0b583f7e2d4c3e2f74f684db88f0754022
StephenBrooks220413 <krammer439@gmail.com>
2021-11-04 07:37:12 -0700

    update

--- week1/js/menu.js
+++ week1/js/menu.js
@@ -1,21 +1,21 @@
-fetch('./menu.json')//file json objects
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                appendData(data);
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-        function appendData(data) {
-            var mainContainer = document.getElementById("myData");
-            for (var i = 0; i < data.results.length; i++) {
-                var items = document.createElement("div");
-                items.innerHTML = ' COLD SANDWICHES: ' + " Turkey: " + data.results[i].coldSandwiches.turkey
-                +  " Ham: " + data.results[i].coldSandwiches.ham +  " Chicken " + data.results[i].coldSandwiches.chicken + ' HOT SANDWICHES: ' + " BLT " + data.results[i].hotSandwiches.blt 
-                + " Chicken " + data.results[i].hotSandwiches.chicken + " GrilledCheese: " + data.results[i].hotSandwiches.grilledCheese + " DESERTS: " + data.results[i].deserts.darkSwirl 
-                + " StrawBerry: " + data.results[i].deserts.strawberry  + " Blueberry: " + data.results[i].deserts.blueberrySwirl + " DRINKS: " + data.results[i].drinks;
-                mainContainer.appendChild(items);
-            }
-        }
+fetch('./store/menu.json')//file json objects
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
+function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.results.length; i++) {
+        var items = document.createElement("div");
+        items.innerHTML = ' COLD SANDWICHES: ' + " Turkey: " + data.results[i].coldSandwiches.turkey
+            +  " Ham: " + data.results[i].coldSandwiches.ham +  " Chicken " + data.results[i].coldSandwiches.chicken + ' HOT SANDWICHES: ' + " BLT " + data.results[i].hotSandwiches.blt
+            + " Chicken " + data.results[i].hotSandwiches.chicken + " GrilledCheese: " + data.results[i].hotSandwiches.grilledCheese + " DESERTS: " + data.results[i].deserts.darkSwirl
+            + " StrawBerry: " + data.results[i].deserts.strawberry  + " Blueberry: " + data.results[i].deserts.blueberrySwirl + " DRINKS: " + data.results[i].drinks;
+        mainContainer.appendChild(items);
+    }
+}
